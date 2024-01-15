2a1f7ac7c92c34058319db14feb6a2558fd714b8
phanminhdat0511 <phanminhdat69@gmail.com>
2024-01-15 20:43:41 +0700

    Ví dụ đầu tiên
    Chỉ đề demo

---
+++ HelloWord/src/Xinchao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào");
+	}
+
+}
