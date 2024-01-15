9c2c17107f872d138e5c9a0a50e4d7e225db3c6f
phanminhdat0511 <phanminhdat69@gmail.com>
2024-01-15 20:45:44 +0700

    new code

--- HelloWord/src/Xinchao.java
+++ HelloWord/src/Xinchao.java
@@ -13,6 +13,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chào");
+		System.out.println("new code");
 	}
 
 }
