9a8748dfc1f3e900ea8d5dd68e4c359eec46ccab
phanminhdat0511 <phanminhdat69@gmail.com>
2024-01-16 20:47:21 +0700

    Bai1

---
+++ LAB1/src/Bai1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		String hoTen;
+		double diem;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Nhập họ và tên: ");
+		hoTen = scan.nextLine();
+		System.out.print("Nhập điểm: ");
+		diem = scan.nextDouble();
+		
+		System.out.printf("%s %f điểm", hoTen, diem);
+
+	}
+
+}
