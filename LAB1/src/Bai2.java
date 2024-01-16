c1c5652fa30d5ef9edd311504fbbd17ab1001d71
phanminhdat0511 <phanminhdat69@gmail.com>
2024-01-16 21:07:58 +0700

    Bài 2

---
+++ LAB1/src/Bai2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		double chieuDai;
+		double chieuRong;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.printf("Nhập chiều dài: ");
+		chieuDai = scan.nextDouble();
+		System.out.printf("Nhập chiều rộng: ");
+		chieuRong = scan.nextDouble();
+		scan.close();
+		
+		System.out.printf("Chu vi hình chữ nhật: %f", (chieuDai + chieuRong)*2);
+		System.out.printf("Diện tích hình chữ nhật: %f", (chieuDai * chieuRong));
+		System.out.printf("Chiều dài của cạnh nhỏ nhất: %f", Math.min(chieuDai, chieuRong));
+	}
+
+}
