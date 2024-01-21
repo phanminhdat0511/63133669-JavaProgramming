7183f146642c2ca2906c14bfd2832f01af558ce5
phanminhdat0511 <phanminhdat69@gmail.com>
2024-01-21 22:29:46 +0700

    Update Bai2.java

--- LAB3/src/Bai2.java
+++ LAB3/src/Bai2.java
@@ -13,6 +13,14 @@ public class Bai2 {
 		for(int i=1; i<=10; i++) {
 			System.out.printf("%d x %d = %d\n", a, i, a*i);
 		}
+		
+		System.out.println("\n\n\nTổng hợp 9 bảng cửu chương:");
+		for(int i=1; i<=9; i++) {
+			System.out.printf("\nBảng cửu chương %d:\n", i);
+			for(int j=1; j<=10; j++) {
+				System.out.printf("%d * %d = %d\n", i, j, i*j);
+			}
+		}
 	}
 	
 }
