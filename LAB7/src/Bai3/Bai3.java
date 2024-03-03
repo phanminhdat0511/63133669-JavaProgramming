278166c7b06477b6f4f09776e30291048ee6ba02
phanminhdat0511 <phanminhdat69@gmail.com>
2024-03-03 13:14:28 +0700

    Update Bai3.java

--- LAB7/src/Bai3/Bai3.java
+++ LAB7/src/Bai3/Bai3.java
@@ -23,10 +23,29 @@ public class Bai3 {
 		
 	}
 	
-	public static void main(String[] args) {
-		SinhVienIT svIT1 = new SinhVienIT("Tên sinh viên", "Ngành", 7.5, 8.5, 9.0);
-		svIT1.xuat();
+	public static class SinhVienBiz extends SinhVienPoly{
+		protected double diemmarketing;
+		protected double diemsales;
 		
+		public SinhVienBiz(String hoTen, String nganh, double diemMarketing, double diemSales) {
+	        super(hoTen, nganh);
+	        this.diemmarketing = diemMarketing;
+	        this.diemsales = diemSales;
+	    }
+		
+		@Override
+		public double getDiem() {
+			return (diemmarketing + diemsales) / 2;
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		SinhVienIT svIT1 = new SinhVienIT("Tran Van A", "Ngành IT", 7.5, 8.5, 9.0);
+		svIT1.xuat();
+		SinhVienBiz svBiz1 = new SinhVienBiz("Nguyen Van B", "Ngành Biz", 8.5, 9.0);
+		svBiz1.xuat();
 	}
 
 }
