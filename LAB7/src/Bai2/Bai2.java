2ce55cfc0cfbb2f779008b4622756cce366949cf
phanminhdat0511 <phanminhdat69@gmail.com>
2024-03-03 13:00:48 +0700

    Create Bai2.java

---
+++ LAB7/src/Bai2/Bai2.java
@@ -0,0 +1,41 @@
+package Bai2;
+
+public class Bai2 {
+	
+	public abstract class SinhVienPoly {
+	    protected String hoTen;
+	    protected String nganh;
+
+	    protected SinhVienPoly(String hoTen, String nganh) {
+	        this.hoTen = hoTen;
+	        this.nganh = nganh;
+	    }
+
+	    public abstract double getDiem();
+
+	    public void getHocLuc() {
+	        if (getDiem() < 5)
+	            System.out.println("Yếu");
+	        else if (getDiem() < 6.6)
+	            System.out.println("Trung bình");
+	        else if (getDiem() < 7.5)
+	            System.out.println("Khá");
+	        else if (getDiem() < 9)
+	            System.out.println("Giỏi");
+	        else
+	            System.out.println("Xuất sắc");
+	    }
+
+	    public void xuat() {
+	        System.out.print(hoTen + nganh + getDiem());
+	        getHocLuc();
+	    }
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
